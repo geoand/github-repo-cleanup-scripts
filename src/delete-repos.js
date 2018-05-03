e00ac962fb380790bfdcc4d53f012e1b8d69cd4d
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2018-05-03 12:52:53 -0400

    Better file handling & logging

--- src/delete-repos.js
+++ src/delete-repos.js
@@ -2,23 +2,24 @@ const axios = require('axios');
 const fs = require('fs');
 const config = require('./config');
 
-const reposForDeletion = require('./repos');
+const reposForDeletion = JSON.parse(fs.readFileSync('repos.json'));
 
 function deleteRepos(repos) {
-  repos.forEach(async repo => {
-    const URL = `https://api.github.com/repos/${repo}`;
-    await axios({
-      method: 'delete',
-      url: URL,
-      params: {
-        access_token: config.access_token,
-      },
-    }).then(() => {
-      console.log(`${repo} deleted!`);
-    }).catch(() => {
-      console.error(`${repo} not found...`);
-    });
-  });
+	console.log(`Deleting ${repos ? repos.length : 0} repos`);
+	repos.forEach(async repo => {
+		const URL = `https://api.github.com/repos/${repo}`;
+		await axios({
+			method: 'delete',
+			url: URL,
+			params: {
+				access_token: config.access_token,
+			},
+		}).then(() => {
+			console.log(`${repo} deleted!`);
+		}).catch(() => {
+			console.error(`${repo} not found...`);
+		});
+	});
 }
 
 deleteRepos(reposForDeletion);
