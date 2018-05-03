4a2d8657110a262e181f9461072793ad35b5201e
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2018-05-03 12:42:07 -0400

    Actually support paging to infinity

--- src/fetch-repos.js
+++ src/fetch-repos.js
@@ -4,30 +4,46 @@ const config = require('./config');
 
 const username = config.github_username;
 const URL = `https://api.github.com/users/${username}/repos`;
-const NUMBER_OF_PAGES = 8; // Your total number of repos / page size of 30. TODO: Remove hardcoding.
 
 async function fetchRepos(url) {
-  const repos = [];
-  for (let i = 1; i <= NUMBER_OF_PAGES; i++) {
-    await axios.get(url, {
-      params: {
-        page: i,
-        access_token: config.access_token,
-      },
-    }).then(res => {
-      const forkedRepos = res.data.filter(repo => repo.fork).map(repo => repo.full_name);
-      console.log(`Fetched ${forkedRepos.length} repos on page ${i}:`);
-      console.log(forkedRepos);
-      repos.push(...forkedRepos);
-    }).catch(err => {
-      console.error(`Error fetching page ${i}: ${err}`);
-    });
-  }
-  return repos;
+	const result = [];
+
+	let getPage = async function(page) {
+		if (!page) page = 1;
+		return axios.get(url, {
+			params: {
+				page: page,
+				access_token: config.access_token,
+			},
+		}).then(res => {
+			repoCount = 0;
+			if (res.data) {
+				const repos = res.data.map(repo => repo.full_name);
+				if (repos) {
+					console.log(`Fetched ${repos.length} repos on page ${page}:`);
+					for (let r of repos) {
+						if (result.indexOf(r) === -1) {
+							result.push(r);
+							console.log(r);
+						}
+					}
+					if (repos.length > 0) {
+						return getPage(page + 1);
+					}
+				}
+			}
+		}).catch(err => {
+			console.error(`Error fetching page ${page}: ${err}`);
+		});
+	}
+
+	return getPage(1).then(() => {
+		return result;
+	});
 }
 
 fetchRepos(URL).then(result => {
-  console.log('Forked repos found:', result.length);
-  console.log(result);
-  fs.writeFileSync('repos.json', JSON.stringify(result, null, 2));
+	console.log('Forked repos found:', result.length);
+	console.log(result);
+	fs.writeFileSync('repos.json', JSON.stringify(result, null, 2));
 });
